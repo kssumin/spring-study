cb33358cc91a4855766989d51c6213dd2c7abc71
kssumin <201566@jnu.ac.kr>
2022-05-14 08:40:51 +0900

    section 5 : 웹 MVC 개발

--- introductory class/hello1-spring/src/main/java/hello1/hello1spring/controller/MemberController.java
+++ introductory class/hello1-spring/src/main/java/hello1/hello1spring/controller/MemberController.java
@@ -1,9 +1,15 @@
 package hello1.hello1spring.controller;
 
+import hello1.hello1spring.domain.Member;
 import hello1.hello1spring.repository.MemoryMemberRepository;
 import hello1.hello1spring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller
 public class MemberController {
@@ -18,5 +24,27 @@ public class MemberController {
     //스프링 컨테이너에 있는 memberService 빈이랑 연결을 시켜주는 역할
     public MemberController(MemberService memberService){
         this.memberService=memberService;
+    }
+
+    @GetMapping("/members/new")
+    public String createForm(){
+        return "members/createMemberForm";
+    }
+
+    @PostMapping("/members/new")
+    public String create(MemberForm form){
+        Member member=new Member();
+        member.setName(form.getName());
+
+        memberService.join(member);
+        return "redirect:/";
+    }
+
+    @GetMapping("/members")
+    public String list(Model model){
+        List<Member> members=memberService.findMembers();
+        model.addAttribute("members",members);
+
+        return "members/memberList";
     }
 }
