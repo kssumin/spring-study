a2343c7cec7f910e9b817d1db6e0716683651822
kssumin <201566@jnu.ac.kr>
2022-05-14 10:13:34 +0900

    section 6 : 순수 Jdbc

--- introductory class/hello1-spring/src/main/java/hello1/hello1spring/SpringConfig.java
+++ introductory class/hello1-spring/src/main/java/hello1/hello1spring/SpringConfig.java
@@ -1,19 +1,33 @@
 package hello1.hello1spring;
 
+import hello1.hello1spring.repository.JdbcMemberRepository;
 import hello1.hello1spring.repository.MemberRepository;
 import hello1.hello1spring.repository.MemoryMemberRepository;
 import hello1.hello1spring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.sql.DataSource;
+
 @Configuration
 public class SpringConfig {
+
+    public DataSource dataSource;
+
+    @Autowired
+    public SpringConfig(DataSource dataSource){
+        this.dataSource=dataSource;
+    }
+
+
     @Bean
     public MemberService memberService(){
         return new MemberService(memberRepository());
     }
     @Bean
     public MemberRepository memberRepository(){
-        return new MemoryMemberRepository();
+        //return new MemoryMemberRepository();
+        return new JdbcMemberRepository(dataSource);
     }
 }
