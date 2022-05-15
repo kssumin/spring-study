c77a4fd37ea4374f6018830d14f244b10e706be0
kssumin <201566@jnu.ac.kr>
2022-05-16 08:28:11 +0900

    section 6 : 스프링 데이터 JPA

--- introductory class/hello1-spring/src/main/java/hello1/hello1spring/SpringConfig.java
+++ introductory class/hello1-spring/src/main/java/hello1/hello1spring/SpringConfig.java
@@ -6,32 +6,29 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import javax.persistence.EntityManager;
-
 @Configuration
 public class SpringConfig {
 
 
 
-    private EntityManager em;
-
+    private final MemberRepository memberRepository;
     @Autowired
-    public SpringConfig(EntityManager em) {
-        this.em = em;
+    public SpringConfig(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
     }
 
 
     @Bean
     public MemberService memberService(){
-        return new MemberService(memberRepository());
-    }
-    @Bean
-    public MemberRepository memberRepository(){
-        //return new MemoryMemberRepository();
-        //return new JdbcMemberRepository(dataSource);
-        //return new JdbcTemplateMemberRepository(dataSource);
-        return new JpaMemberRepository(em);
+        return new MemberService(memberRepository);
     }
 
-    //스프링 통합 테스트(DB+스프링 부트까지 연결)를 만들어 놓았기에 웹 어플리케이션을 띄어서 검증할 필요가 없다.
+//    @Bean
+//    public MemberRepository memberRepository(){
+//        //return new MemoryMemberRepository();
+//        //return new JdbcMemberRepository(dataSource);
+//        //return new JdbcTemplateMemberRepository(dataSource);
+//        return new JpaMemberRepository(em);
+//    }
+    
 }
