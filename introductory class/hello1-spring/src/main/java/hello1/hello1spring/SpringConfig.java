71ecf36bb49d6feea6b2205cba34f75daf0da6e9
kssumin <201566@jnu.ac.kr>
2022-05-16 07:31:02 +0900

    section 6: 스프링 JPA

--- introductory class/hello1-spring/src/main/java/hello1/hello1spring/SpringConfig.java
+++ introductory class/hello1-spring/src/main/java/hello1/hello1spring/SpringConfig.java
@@ -1,25 +1,23 @@
 package hello1.hello1spring;
 
-import hello1.hello1spring.repository.JdbcMemberRepository;
-import hello1.hello1spring.repository.JdbcTemplateMemberRepository;
-import hello1.hello1spring.repository.MemberRepository;
-import hello1.hello1spring.repository.MemoryMemberRepository;
+import hello1.hello1spring.repository.*;
 import hello1.hello1spring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.jdbc.core.JdbcTemplate;
 
-import javax.sql.DataSource;
+import javax.persistence.EntityManager;
 
 @Configuration
 public class SpringConfig {
 
-    public DataSource dataSource;
+
+
+    private EntityManager em;
 
     @Autowired
-    public SpringConfig(DataSource dataSource){
-        this.dataSource=dataSource;
+    public SpringConfig(EntityManager em) {
+        this.em = em;
     }
 
 
@@ -31,7 +29,8 @@ public class SpringConfig {
     public MemberRepository memberRepository(){
         //return new MemoryMemberRepository();
         //return new JdbcMemberRepository(dataSource);
-        return new JdbcTemplateMemberRepository(dataSource);
+        //return new JdbcTemplateMemberRepository(dataSource);
+        return new JpaMemberRepository(em);
     }
 
     //스프링 통합 테스트(DB+스프링 부트까지 연결)를 만들어 놓았기에 웹 어플리케이션을 띄어서 검증할 필요가 없다.
--- introductory class/hello1-spring/src/main/java/hello1/hello1spring/domain/Member.java
+++ introductory class/hello1-spring/src/main/java/hello1/hello1spring/domain/Member.java
@@ -1,7 +1,16 @@
 package hello1.hello1spring.domain;
 
+import org.springframework.transaction.reactive.GenericReactiveTransaction;
+
+import javax.persistence.*;
+
+@Entity //JPA가 관리하는 Entity이다
+
 public class Member {
+    @Id//애는 PK
+    @GeneratedValue(strategy = GenerationType.IDENTITY)//디비가 아이디 값을 자동으로 값을 넣어줌 -> Identity 전략?
     private Long id;
+    //@Column(name="username") -> DB의 컬럼명을 바꿔준다.
     private String name;
 
     public Long getId() {
@@ -20,5 +29,7 @@ public class Member {
         this.name = name;
     }
 
+    //이러한 애노테이션을 가지고 DB와 매핑을 한다.
+    //이 정보를 가지고 insert,delete 같은 걸 한다.
 
 }
