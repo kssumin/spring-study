b28e731ba3b270f7d5cd6be5a27ab0dedc53050e
kssumin <201566@jnu.ac.kr>
2022-05-16 00:00:16 +0900

    section 6 : 스프링 JdbcTemplate

--- introductory class/hello1-spring/src/main/java/hello1/hello1spring/SpringConfig.java
+++ introductory class/hello1-spring/src/main/java/hello1/hello1spring/SpringConfig.java
@@ -1,12 +1,14 @@
 package hello1.hello1spring;
 
 import hello1.hello1spring.repository.JdbcMemberRepository;
+import hello1.hello1spring.repository.JdbcTemplateMemberRepository;
 import hello1.hello1spring.repository.MemberRepository;
 import hello1.hello1spring.repository.MemoryMemberRepository;
 import hello1.hello1spring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 import javax.sql.DataSource;
 
@@ -28,6 +30,9 @@ public class SpringConfig {
     @Bean
     public MemberRepository memberRepository(){
         //return new MemoryMemberRepository();
-        return new JdbcMemberRepository(dataSource);
+        //return new JdbcMemberRepository(dataSource);
+        return new JdbcTemplateMemberRepository(dataSource);
     }
+
+    //스프링 통합 테스트(DB+스프링 부트까지 연결)를 만들어 놓았기에 웹 어플리케이션을 띄어서 검증할 필요가 없다.
 }
